cfb319d48b56730b17d11b4317392f6488756771
varut siriparkornchai <61010950@kmitl.ac.th>
2020-01-28 17:05:33 +0700

    Update week1.py

--- week1.py
+++ week1.py
@@ -2,7 +2,7 @@ def alphabet_position(sentence):
     contain = ""
     sentence = sentence.lower()                                   # convert sentence to lowercase
     for a in sentence:
-        if a.isalpha() == True:
+        if a.isalpha() == True:                                   # .isalpha means Is that alphabet?
             contain += (str(ord(a)-96))+(" ")                    
     return print(contain.strip())                                  # cut blank first and last letter
 
