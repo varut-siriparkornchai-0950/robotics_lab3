e9337ff72a9e3121a68d50d5029bd8794dbade9c
varut siriparkornchai <61010950@kmitl.ac.th>
2020-02-03 15:52:03 +0700

    Update week2.py

--- week2.py
+++ week2.py
@@ -1,14 +1,8 @@
 def validate_pin(pin):
     let = len(pin)                                          # len(pin) -> counting letter in 'pin'
-    for a in pin:
-        if a in range(10):
-            if pin.isdigit() == True and (let == 4 or let == 6):    # .isdigit() -> check pin, Is that digital(number)?
-                 print(True)
-            else:
-                print(False)
-        else:
-            break
-    print(False)
-
+    if pin.isdigit() == True and (let == 4 or let == 6):    # .isdigit() -> check pin, Is that digital(number)?
+        return True
+    else:
+        return False
 
 
