b8e6dca9e199fc0c6ce8846b034924af638146ab
varut siriparkornchai <61010950@kmitl.ac.th>
2020-02-17 22:42:05 +0700

    update

--- thresholded image.py
+++ thresholded image.py
@@ -1,3 +1,8 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+
+img = cv2.imread('container.jpg', 0)                            #import image and convert to GRAY.SCALE
+ret,thresh1 = cv2.threshold(img,85,255,cv2.THRESH_BINARY)       #img, __pixel value under this will be BLACK__, __maximum value of WHITE__
+cv2.imshow("Processed image", thresh1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
